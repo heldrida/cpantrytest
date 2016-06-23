6e50ca47f092c5002dd3fd4d9f5978dbae386ac8
Helder <helder@lamoulade.com>
2016-06-23 18:43:59 +0100

    improved assertions

--- tests/test.js
+++ tests/test.js
@@ -1,37 +1,49 @@
 describe('The FetchData service', function() {
 
-   var mainCtrl, dataFetcher, $httpBackend, $rootScope, createController, authRequestHandler;
-   var fooRes = {
-		success: true
-	};
+	var dataFetcher, $httpBackend;
 
-  beforeEach(angular.mock.module("myApp"))
+	beforeEach(angular.mock.module("myApp"));
 
-  beforeEach(inject(function(_dataFetcher_, _$httpBackend_) {
+	beforeEach(inject(function(_dataFetcher_, _$httpBackend_) {
+		dataFetcher = _dataFetcher_;
+		$httpBackend = _$httpBackend_;
+	}));
 
-    dataFetcher = _dataFetcher_;
-    $httpBackend = _$httpBackend_;
-
-    $httpBackend
-      .when('GET', 'https://api.citypantry.com/packages/2140')
-      .respond(fooRes);
-
-  }));
-
-   afterEach(function() {
-     $httpBackend.verifyNoOutstandingExpectation();
-     $httpBackend.verifyNoOutstandingRequest();
-   });
-
-  it('should fetch data', function () {
-
-	dataFetcher.getPackageByHumanId(2140).then(function (res) {
-		expect(res.success).toBe(fooRes.success);
+	afterEach(function() {
+		$httpBackend.verifyNoOutstandingExpectation();
+		$httpBackend.verifyNoOutstandingRequest();
 	});
 
-	$httpBackend.flush();
+	it('should fetch data', function () {
 
-   });
+		var status, data,
+			pid = 2140,
+			api = 'https://api.citypantry.com/packages';
+
+		function successHandler(res){
+			status = 'success';
+			data = res.data;
+			expect(res.success).toEqual(true);
+		}
+
+		function errorHandler(){
+			status = 'error'
+		}
+
+		$httpBackend
+		 .when('GET', api + '/' + pid)
+		 .respond({ success: true });
+
+		$httpBackend
+		 .when('GET', api + '/' + pid)
+		 .respond(500, 'An error has occured.');
+
+		dataFetcher.getPackageByHumanId(pid).then(successHandler, errorHandler);
+
+		$httpBackend.flush();
+
+		expect(status).toEqual('success');
+
+	});
 
 });
-
